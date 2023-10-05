c228cd4bdd86b1b58e70a9214a57d83a872c7d51
keeprok <dldudfhr425@gmail.com>
2023-10-05 22:09:33 +0900

    docs js01-15~18

---
+++ j-s연습/j-s하루100제/js01/11~20/16.js
@@ -0,0 +1,13 @@
+const im = "거꾸로";
+const mi = im.split("").reverse().join("");
+console.log(mi);
+//다시 연습 split 같은경우여러개쓸때는 .추가로쓰면됨 @@@
+// const n = prompt('입력하세요.');
+
+// const reverseString = n.split('').reverse().join('');
+// /*
+// * split() 메서드는 문자열을 배열로 만들어 반환하고,
+// * reverse() 메서드는 배열의 순서를 반전하며,
+// * join() 메서드는 원소를 모두 붙여 문자열로 반환합니다.
+// */
+// console.log(reverseString);
